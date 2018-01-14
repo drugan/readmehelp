b55ec751e79ecbff4995e266d58ca9eea6979bd8
drugan <proshins@gmail.com>
2018-01-14 21:13:33 +0200

    Issue #2936632: Display module's admin links on the README Help page

--- src/Controller/ReadmeHelpController.php
+++ src/Controller/ReadmeHelpController.php
@@ -15,7 +15,8 @@ class ReadmeHelpController extends HelpController {
   public function helpPage($name) {
     $build = [];
     $self = $name == 'readmehelp';
-    $dependencies = system_get_info('module', $name)['dependencies'];
+    $info = system_get_info('module', $name);
+    $dependencies = isset($info['dependencies']) ? $info['dependencies'] : [];
     $depender = $self || in_array('readmehelp', $dependencies) || in_array('drupal:readmehelp', $dependencies);
     // Allow dependers to override default behaviour not displaying README
     // markdown file automatically and instead calling a regular hook_help() in
@@ -32,6 +33,28 @@ class ReadmeHelpController extends HelpController {
         ],
         '#markup' => $converter->convertMarkdownFile($name),
       ];
+
+      // Only print list of administration pages if the module in question has
+      // any such pages associated with it.
+      $admin_tasks = system_get_module_admin_tasks($name, $info);
+      if (!empty($admin_tasks)) {
+        $module_name = $this->moduleHandler()->getName($name);
+        $links = [];
+        foreach ($admin_tasks as $task) {
+          $link['url'] = $task['url'];
+          $link['title'] = $task['title'];
+          $links[] = $link;
+        }
+        $build['links'] = [
+          '#theme' => 'links__help',
+          '#heading' => [
+            'level' => 'h3',
+            'text' => $this->t('@module administration pages', ['@module' => $module_name]),
+          ],
+          '#links' => $links,
+        ];
+      }
+
       return $build;
     }
     else {
